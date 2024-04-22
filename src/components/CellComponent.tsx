c64f18549ee608a9e1b0aad8e9ed3b9cf32393c5
Savelev Andrey <needway@list.ru>
2024-04-22 10:25:05 +0300

    Movement(Rook,Queen,Bishop DONE)
    some component changes( available cells) and movement logic

--- src/components/CellComponent.tsx
+++ src/components/CellComponent.tsx
@@ -3,12 +3,19 @@ import { Cell } from '../models/Cell'
 
 interface CellProps {
     cell: Cell;
+    selected: boolean;
+    click: (cell: Cell) => void;
 }
 
-export const CellComponent: FC<CellProps>= ({cell}) => {
+export const CellComponent: FC<CellProps>= ({cell, selected, click}) => {
   return (
     <div
-    className={['cell',cell.color].join(' ')}>
+    className={['cell',cell.color, selected ? 'selected' : ''].join(' ')}
+    onClick={() => click(cell)}
+    style={{background: cell.available && cell.figure ? 'green' : ''}}> 
+    {/*пока инлайн стиль */}
+
+      {cell.available && !cell.figure && <div  className={'available'}/>}
         {cell.figure?.logo && <img src={cell.figure.logo}></img>}
         
         </div>
